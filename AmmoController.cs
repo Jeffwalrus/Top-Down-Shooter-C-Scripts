2f720e4f9a6a82b90d0bbba7f30684c8e92da365
Jeffwalrus <44712173+Jeffwalrus@users.noreply.github.com>
2018-12-10 14:25:21 -0500

    Add files via upload

---
+++ AmmoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoController : MonoBehaviour
+{
+    public int ammo = 30;
+    public Text uitext;
+
+    void Update()
+    {
+        if (uitext != null)
+        {
+            uitext.text = ammo.ToString();
+        }
+    }
+}
---
+++ UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text AmmoCount;
+    private GunController gunCon;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        AmmoCount.text = "Ammo: " + gunCon.ammoNo + "/30";
+	}
+}
